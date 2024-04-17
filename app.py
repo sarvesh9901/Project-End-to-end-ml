e1fb3427617bf0f30faec19822ac6b96650f88c0
sarvesh9901 <sarveshkaranjkar516@gmail.com>
2024-04-17 19:45:50 +0530

    hyperparameter tuning

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_injection import DataIngestion
 from src.mlproject.components.data_injection import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
-#from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 import sys
 if __name__ == '__main__':
     logging.info('Running')
@@ -15,6 +15,9 @@ if __name__ == '__main__':
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        ## Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
     except Exception as e:
         logging.info("exception found")
         raise CustomException(e,sys)
